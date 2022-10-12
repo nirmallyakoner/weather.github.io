023b02d55580b50afea1466e7bc46fe77b7a89f1
nirmallyakoner <79015427+nirmallyakoner@users.noreply.github.com>
2022-10-12 11:48:21 +0530

    weather webpage

---
+++ script.js
@@ -0,0 +1,36 @@
+let weather={
+    apikey:"8eb1f3ecc27c9a912a2f1f488968cd1b",
+    fetchWeather: function(city){
+        fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" 
+        +city 
+        +"&units=metric&appid=" 
+        +this.apikey
+    )
+        .then((response)=> response.json())
+        .then((data)=>this.displayWeather(data));
+    },
+    displayWeather: function(data){ 
+        const{name}=data;
+        const{icon, description}=data.weather[0];
+        const{temp,humidity}=data.main;
+        const{speed}=data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText="Weather in "+name;
+        document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+ icon +".png";
+        document.querySelector(".description").innerText=description;
+        document.querySelector(".temp").innerText=temp +"°C ";
+        document.querySelector(".humidity").innerText="humidity: " + humidity + "%" ;
+        document.querySelector(".wind").innerText="Wind Speed: " + speed+ "km/h";
+    },
+
+
+    search: function(){
+      this.fetchWeather(document.querySelector(".search-bar").value);        
+    }
+}
+
+
+document.querySelector(".search button").addEventListener("click", function(){
+   weather.search();
+})
